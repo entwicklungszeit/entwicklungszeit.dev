fcb4e1977cd50b3cdcd4b92d97f100565e7adb97
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-04-08 22:51:22 +0200

    feat(host): improve carousel items

--- src/data/host.ts
+++ src/data/host.ts
@@ -1,18 +1,18 @@
 export const pages = [
   {
     content:
-      'Hi, ich bin Gregor und freu mich sehr, dass du hier bist. Dinge Automatisieren find ich klasse, aber unter uns gesprochen finde ich es noch viel schöner Softwareentwicklung zu einem kreativen Erlebnis zu machen, das Allen im Team Freude bereitet. Es darf gelacht werden, es dürfen neue Wege ausprobiert werden und gemeinsam entdeckt jedes Team das "Engineering" für sich als lebenden Prozess der Verbesserung.',
+      'Hi, ich bin Gregor und freue mich sehr, dass du hier bist. Dinge Automatisieren find ich klasse, aber unter uns gesprochen finde ich es noch viel schöner Softwareentwicklung zu einem kreativen Erlebnis zu machen, das Allen im Team Freude bereitet. Es darf gelacht werden, es dürfen neue Wege ausprobiert werden und gemeinsam entdeckt jedes Team das "Engineering" für sich als lebenden Prozess der Verbesserung.',
     caption: 'Team'
   },
   {
     content:
-      'Das schönste ist, wenn das Team ein gemeinsames Flow-Erlebnis erzielt und Dinge liefert, die die Erwartungen ihrer Kunden übertreffen. In den letzten 17 Jahren durfte ich dazu viel Lernen. Nicht nur als Entwickler. Ich engagiere mich in der Software Community als Sprecher, habe für mehr als 900 Teilnehmer Workshops gegeben, habe an Architekturen für Konzernen und großen mittelständischen Unternehmen mitgewirkt.',
+      'Das schönste ist, wenn das Team ein gemeinsam an Software "tüftelt und Lösungen bereitstellt, die die Erwartungen ihrer Kunden übertreffen. In den letzten 17 Jahren durfte ich dazu viel Lernen. Nicht nur als Entwickler. Ich engagiere mich in der Software Community als Sprecher, habe für mehr als 900 Teilnehmer Workshops gegeben, habe an Architekturen für Konzernen und großen mittelständischen Unternehmen mitgewirkt.',
     caption: 'Erfahrungen'
   },
   {
     content:
-      'Ich habe mich zu dem im Vertrieb ausbilden lassen, weil ich unbedingt dich nicht-technische Ebene mit ihren Möglichkeiten und Herausforderungen kennenlernen wollte. So konnte ich auch strategisch neun Jahre als CTO Geschäftsprozess und Abläufe gestalten und dafür sorgen, dass unser Entwicklerteam stetig wachsen und sich verbessern kann, damit unsere Produkte zu etwas besonderen werden.',
-    caption: 'Perspektivwechsel'
+      'Wirtschaftlichkeit in der Softwareentwicklung und technische Eleganz bilden gemeinsam ein Schwungrad für erfolgreiche Systeme. Nur manchmal benötigt es einen Schubs in die richtige Richtung. Neben Führung und dem Aufbau technischer Skills, muss die Team-Dynamik verstanden, akzeptiert und richtig gefördert werden.',
+    caption: 'Wirtschaftlichkeit'
   },
   {
     content:
