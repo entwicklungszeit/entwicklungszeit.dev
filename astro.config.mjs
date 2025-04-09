e8fa54e40a4187277344661f15ba1df89d97469f
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-04-09 21:54:29 +0200

    feat(hero): list podcast platforms

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   integrations: [tailwind()],
+  experimental: { svg: true },
   image: {
     service: { entrypoint: 'astro/assets/services/sharp' }
   }
