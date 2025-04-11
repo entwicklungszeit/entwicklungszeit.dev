7b321b9980e95e5a33833d6b46762aa83fff7a6d
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-04-11 20:40:17 +0200

    chore: add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
+import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   experimental: { svg: true },
   image: {
     service: { entrypoint: 'astro/assets/services/sharp' }
