08c73bb32a23cc486c038939ed9a475db0d88850
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-04-11 21:05:39 +0200

    fix(faq): correct spelling

--- src/data/faqData.ts
+++ src/data/faqData.ts
@@ -12,18 +12,18 @@ export const faqs: FaqItem[] = [
   },
   {
     question: 'Was ist der Unterschied zwischen Mentoring und Coaching?',
-    answer: `In einem Mentoring bekommst du eine genaue Anleitung, wie du etwas besser machen kannst. Stell es dir wie einen Blue-Print vor. Das eignet sich in vielen Disziplinen der Softwareentwicklung.
+    answer: `In einem Mentoring bekommst du eine genaue Anleitung, wie du etwas besser machen kannst. Stell es dir wie einen Blueprint vor. Das eignet sich in vielen Disziplinen der Softwareentwicklung.
 
-Im Coaching gehe ich mit dir in die Tiefe und löse Blockaden auf, die dich hindern im Beruf noch erfolgreicher zu sein. Wir gehen auf Kommunikation und Persönlichkeit ein. Wir stärken deinen Positionierung. Wir arbeiten an Spannungen und Konflikten mit dem Ziel diese aufzulösen.`
+Im Coaching gehe ich mit dir in die Tiefe und löse Blockaden auf, die dich daran hindern, im Beruf noch erfolgreicher zu sein. Wir gehen auf Kommunikation und Persönlichkeit ein. Wir stärken deine Positionierung. Wir arbeiten an Spannungen und Konflikten mit dem Ziel, diese aufzulösen.`
   },
   {
     question: 'Warum der Mix aus Mentoring und Coaching?',
-    answer: `Sicher hast du häufig Mentoring kennengelernt. Teste so, schreibe Commit-Nachrichten so, baue die Architektur so. Dabei helfe ich dir gern. Ich möchte dir darüber hinaus helfen, dass du deinen eigenen Weg gehen kannst. Projekte und Produktentwicklung scheitern selten an der Technologie, sondern an Zwischenmenschlichen Beziehungen, unausgesprochenen Problemen. Leider passiert das sehr oft. Hier gehe ich mit dir zusammen in deine Situationen rein und helfe dir sie aufzulösen,- für dich und für dein Team.`
+    answer: `Sicher hast du häufig Mentoring kennengelernt: Teste so, schreibe Commit-Nachrichten so, baue die Architektur so. Dabei helfe ich dir gern. Ich möchte dir darüber hinaus helfen, dass du deinen eigenen Weg gehen kannst. Projekte und Produktentwicklung scheitern selten an der Technologie, sondern an zwischenmenschlichen Beziehungen und unausgesprochenen Problemen. Leider passiert das sehr oft. Hier gehe ich mit dir zusammen in deine Situationen hinein und helfe dir, sie aufzulösen – für dich und für dein Team.`
   },
   {
     question:
-      'Warum nur ein Call? - Was ist, wenn ich ein akutes Problem habe.',
-    answer: `Erfahrungsgemäß gibt ein Call, bei dem ich übrigens nicht auf die Minute schaue genügend Aufgaben für eine Woche. Es können auch Situationen entstehen, über die du für dich nachdenken musst. Veränderung ist nicht immer ein Spaziergang, sondern erfordert Mut. Ich lass dich dabei aber nicht alleine. Du erhältst einen Zugang, sodass du mir jederzeit kontaktieren kannst und ich stehe dir mit Rat und Tat zur Seite.`
+      'Warum nur ein Call? - Was ist, wenn ich ein akutes Problem habe?',
+    answer: `Erfahrungsgemäß gibt ein Call, bei dem ich übrigens nicht auf die Minute schaue, genügend Aufgaben für eine Woche. Es können auch Situationen entstehen, über die du für dich nachdenken musst. Veränderung ist nicht immer ein Spaziergang, sondern erfordert Mut. Ich lasse dich dabei aber nicht alleine. Du erhältst einen Zugang, sodass du mich jederzeit kontaktieren kannst, und ich stehe dir mit Rat und Tat zur Seite.`
   },
   {
     question: 'Wie kann ich unsere Live-Calls nacharbeiten?',
