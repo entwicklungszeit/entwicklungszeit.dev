72cd143f665bab88da7d429f7cd23e0dfc1d0275
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-04-11 21:03:28 +0200

    feat: add faq

---
+++ src/data/faqData.ts
@@ -0,0 +1,32 @@
+import type { FaqItem } from '../types/faq';
+
+export const faqs: FaqItem[] = [
+  {
+    question: 'Wie läuft das Impact Developer-Programm genau ab?',
+    answer: [
+      'Jede Woche findet ein Live-Call statt',
+      'Wir besprechen aktuelle Themen, setzen Prioritäten',
+      'Wir führen ein technisches Mentoring durch',
+      'Wir führen ein Coaching für deine Positionierung durch'
+    ]
+  },
+  {
+    question: 'Was ist der Unterschied zwischen Mentoring und Coaching?',
+    answer: `In einem Mentoring bekommst du eine genaue Anleitung, wie du etwas besser machen kannst. Stell es dir wie einen Blue-Print vor. Das eignet sich in vielen Disziplinen der Softwareentwicklung.
+
+Im Coaching gehe ich mit dir in die Tiefe und löse Blockaden auf, die dich hindern im Beruf noch erfolgreicher zu sein. Wir gehen auf Kommunikation und Persönlichkeit ein. Wir stärken deinen Positionierung. Wir arbeiten an Spannungen und Konflikten mit dem Ziel diese aufzulösen.`
+  },
+  {
+    question: 'Warum der Mix aus Mentoring und Coaching?',
+    answer: `Sicher hast du häufig Mentoring kennengelernt. Teste so, schreibe Commit-Nachrichten so, baue die Architektur so. Dabei helfe ich dir gern. Ich möchte dir darüber hinaus helfen, dass du deinen eigenen Weg gehen kannst. Projekte und Produktentwicklung scheitern selten an der Technologie, sondern an Zwischenmenschlichen Beziehungen, unausgesprochenen Problemen. Leider passiert das sehr oft. Hier gehe ich mit dir zusammen in deine Situationen rein und helfe dir sie aufzulösen,- für dich und für dein Team.`
+  },
+  {
+    question:
+      'Warum nur ein Call? - Was ist, wenn ich ein akutes Problem habe.',
+    answer: `Erfahrungsgemäß gibt ein Call, bei dem ich übrigens nicht auf die Minute schaue genügend Aufgaben für eine Woche. Es können auch Situationen entstehen, über die du für dich nachdenken musst. Veränderung ist nicht immer ein Spaziergang, sondern erfordert Mut. Ich lass dich dabei aber nicht alleine. Du erhältst einen Zugang, sodass du mir jederzeit kontaktieren kannst und ich stehe dir mit Rat und Tat zur Seite.`
+  },
+  {
+    question: 'Wie kann ich unsere Live-Calls nacharbeiten?',
+    answer: `Du erhältst eine Aufzeichnung unserer Calls und eine Nacharbeitung von mir, die du für dich in Ruhe durchgehen kannst. Auch hier: Frag mich jederzeit, wenn etwas unklar ist.`
+  }
+];
