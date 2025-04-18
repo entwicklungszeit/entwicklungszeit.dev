ee12b780be6a678c25a95fa65dba9f813b959a2f
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-04-18 23:22:33 +0200

    chore: remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind()],
   experimental: { svg: true },
   image: {
     service: { entrypoint: 'astro/assets/services/sharp' },
